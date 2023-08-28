abb14f8f4cc31dc69edf95154149ce99f3a8a8d1
Frederique Andolfatto <pk37fki@eduvaud.ch>
2023-08-28 16:25:09 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    user = input("Es-tu parti en vacances?")
+    if user.lower() == "oui":
+        print("quelle chance!")
+    else:
+        print("c'est bien aussi.")
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
